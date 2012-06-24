c7c8204d3b4b43caae53382cffc52d322829b32d
Michael Kristofik <kristo605@gmail.com>
2012-06-23 22:57:30 -0500

    Initial checkin for SDL hello world program.

---
+++ src/test.cpp
@@ -0,0 +1,105 @@
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int, char **)  // 2-arg form is required by SDL
+{
+    if (SDL_Init(SDL_INIT_VIDEO) == -1) {
+        std::cerr << "Error initializing SDL: " << SDL_GetError();
+        return EXIT_FAILURE;
+    }
+    atexit(SDL_Quit);
+
+    if (TTF_Init() < 0) {
+        std::cerr << "Error initializing SDL_ttf: " << TTF_GetError();
+        return EXIT_FAILURE;
+    }
+    atexit(TTF_Quit);
+
+    SDL_Surface *screen = SDL_SetVideoMode(640, 480, 0, SDL_SWSURFACE);
+    if (screen == nullptr) {
+        std::cerr << "Error setting video mode: " << SDL_GetError();
+        return EXIT_FAILURE;    
+    }
+
+    SDL_Surface *temp = IMG_Load("../avatar.png");
+    if (temp == nullptr) {
+        std::cerr << "Error loading image from disk: " << IMG_GetError();
+        return EXIT_FAILURE;
+    }
+    SDL_Surface *picture = SDL_DisplayFormatAlpha(temp);
+    SDL_FreeSurface(temp);
+    if (picture == nullptr) {
+        std::cerr << "Error converting to display format: " << IMG_GetError();
+        return EXIT_FAILURE;
+    }
+    SDL_Rect dest = {300, 200, 0, 0};
+    if (SDL_BlitSurface(picture, nullptr, screen, &dest) < 0) {
+        std::cerr << "Warning: error drawing picture to screen: " << SDL_GetError();
+    }
+
+    TTF_Font *font = TTF_OpenFont("../DejaVuSans.ttf", 12);
+    if (font == nullptr) {
+        std::cerr << "Error loading font: " << TTF_GetError();
+        SDL_FreeSurface(picture);
+        return EXIT_FAILURE;
+    }
+    int hint = TTF_GetFontHinting(font);
+    std::cout << "Curious about font hinting, we have: " << hint << '\n';
+    SDL_Surface *text = TTF_RenderText_Blended(font, "Hello, world!", SDL_Color({255, 255, 255, 0}));
+    if (text == nullptr) {
+        std::cerr << "Error rendering text: " << TTF_GetError();
+        SDL_FreeSurface(picture);
+        TTF_CloseFont(font);
+        return EXIT_FAILURE;
+    }
+    dest = {285, 260, 0, 0};
+    if (SDL_BlitSurface(text, nullptr, screen, &dest) < 0) {
+        std::cerr << "Warning: error drawing text to screen: " << SDL_GetError();
+    }
+    SDL_FreeSurface(text);
+    text = TTF_RenderText_Shaded(font, "Hello, world!", SDL_Color({255, 255, 255, 0}), SDL_Color({0, 0, 0, 0}));
+    if (text == nullptr) {
+        std::cerr << "Error rendering text #2: " << TTF_GetError();
+        SDL_FreeSurface(picture);
+        TTF_CloseFont(font);
+        return EXIT_FAILURE;
+    }
+    dest = {285, 280, 0, 0};
+    if (SDL_BlitSurface(text, nullptr, screen, &dest) < 0) {
+        std::cerr << "Warning: error drawing text to screen: " << SDL_GetError();
+    }
+    SDL_FreeSurface(text);
+    text = TTF_RenderText_Solid(font, "Hello, world!", SDL_Color({255, 255, 255, 0}));
+    if (text == nullptr) {
+        std::cerr << "Error rendering text #2: " << TTF_GetError();
+        SDL_FreeSurface(picture);
+        TTF_CloseFont(font);
+        return EXIT_FAILURE;
+    }
+    dest = {285, 300, 0, 0};
+    if (SDL_BlitSurface(text, nullptr, screen, &dest) < 0) {
+        std::cerr << "Warning: error drawing text to screen: " << SDL_GetError();
+    }
+
+    SDL_UpdateRect(screen, 0, 0, 0, 0);
+
+    bool isDone = false;
+    SDL_Event event;
+    while (!isDone) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isDone = true;
+            }
+        }
+        SDL_Delay(1);
+    }
+
+    SDL_FreeSurface(picture);
+    SDL_FreeSurface(text);
+    TTF_CloseFont(font);
+    return EXIT_SUCCESS;
+}
