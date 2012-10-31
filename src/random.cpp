2b5580b3c871b0e0cfd87b5df0bdda3bd96b33a9
Michael Kristofik <kristo605@gmail.com>
2012-10-30 22:13:05 -0500

    Refactor calls to SDL_BlitSurface.

--- src/random.cpp
+++ src/random.cpp
@@ -2,6 +2,7 @@
 #include "SDL_image.h"
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -20,6 +21,17 @@ int terrain[] = {0,0,0,0,0,0,0,0,0,0,2,2,2,2,3,3,
                  5,5,5,0,0,0,1,1,1,1,1,1,1,1,0,0,
                  5,5,5,5,1,1,1,1,1,1,1,1,1,1,0,0,
                  5,5,5,5,1,1,1,1,1,1,1,1,1,1,0,0};
+
+SDL_Surface *screen = nullptr;
+
+void sdlBlit(const SdlSurface &surf, Sint16 x, Sint16 y)
+{
+    assert(screen != nullptr);
+    SDL_Rect dest = {x, y, 0, 0};
+    if (SDL_BlitSurface(surf.get(), nullptr, screen, &dest) < 0) {
+        std::cerr << "Warning: error drawing to screen: " << SDL_GetError();
+    }
+}
 
 SdlSurface sdlLoadImage(const char *filename)
 {
@@ -61,7 +73,7 @@ extern "C" int SDL_main(int, char **)  // 2-arg form is required by SDL
         std::cerr << "Warning: error loading icon file: " << IMG_GetError();
     }
 
-    SDL_Surface *screen = SDL_SetVideoMode(882, 684, 0, SDL_SWSURFACE);
+    screen = SDL_SetVideoMode(882, 684, 0, SDL_SWSURFACE);
     if (screen == nullptr) {
         std::cerr << "Error setting video mode: " << SDL_GetError();
         return EXIT_FAILURE;    
@@ -75,72 +87,47 @@ extern "C" int SDL_main(int, char **)  // 2-arg form is required by SDL
     tiles.emplace_back(sdlLoadImage("../img/snow.png"));
     tiles.emplace_back(sdlLoadImage("../img/desert.png"));
     tiles.emplace_back(sdlLoadImage("../img/water.png"));
-    SDL_Rect dest = {0, 0, 0, 0};
 
     // Display even-numbered columns.
     for (Sint16 x = 0; x < mapWidth; x += 2) {
         for (Sint16 y = 0; y < mapHeight; ++y) {
-            dest.x = x * hexSize * 0.75;
-            dest.y = y * hexSize;
             auto terrainIndex = terrain[y * mapWidth + x];
-            if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-                std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-            }
+            sdlBlit(tiles[terrainIndex], x * hexSize * 0.75, y * hexSize);
         }
     }
 
     // Display odd-numbered columns.
     for (Sint16 x = 1; x < mapWidth; x += 2) {
         for (Sint16 y = 0; y < mapHeight; ++y) {
-            dest.x = x * hexSize * 0.75;
-            dest.y = (y * hexSize) + (hexSize * 0.5);
             auto terrainIndex = terrain[y * mapWidth + x];
-            if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-                std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-            }
+            sdlBlit(tiles[terrainIndex], x * hexSize * 0.75, (y+0.5) * hexSize);
         }
     }
 
     // Overdraw so we don't get jagged edges.
     for (Sint16 y = -1; y < mapHeight; ++y) {  // left edge, x = -1
-        dest.x = -0.75 * hexSize;
-        dest.y = (y * hexSize) + (hexSize * 0.5);
         int terrainX = 0;
         int terrainY = std::min(y + 1, mapHeight - 1);
         auto terrainIndex = terrain[terrainY * mapWidth + terrainX];
-        if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-            std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-        }
+        sdlBlit(tiles[terrainIndex], -0.75 * hexSize, (y+0.5) * hexSize);
     }
     for (Sint16 x = 1; x < mapWidth; x += 2) {  // top edge, y = -1
-        dest.x = x * hexSize * 0.75;
-        dest.y = -0.5 * hexSize;
         int terrainX = x;
         int terrainY = 0;
         auto terrainIndex = terrain[terrainY * mapWidth + terrainX];
-        if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-            std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-        }
+        sdlBlit(tiles[terrainIndex], x * hexSize * 0.75, -0.5 * hexSize);
     }
     for (Sint16 y = 0; y < mapHeight + 1; ++y) {  // right edge, x = mapWidth
-        dest.x = mapWidth * hexSize * 0.75;
-        dest.y = y * hexSize;
         int terrainX = mapWidth - 1;
         int terrainY = std::min<int>(y, mapHeight - 1);
         auto terrainIndex = terrain[terrainY * mapWidth + terrainX];
-        if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-            std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-        }
+        sdlBlit(tiles[terrainIndex], mapWidth * hexSize * 0.75, y * hexSize);
     }
     for (Sint16 x = 0; x < mapWidth; x += 2) {  // bottom edge, y = mapHeight
-        dest.x = x * hexSize * 0.75;
-        dest.y = mapHeight * hexSize;
         int terrainX = x;
         int terrainY = mapHeight - 1;
         auto terrainIndex = terrain[terrainY * mapWidth + terrainX];
-        if (SDL_BlitSurface(tiles[terrainIndex].get(), nullptr, screen, &dest) < 0) {
-            std::cerr << "Warning: error drawing grass hex to screen: " << SDL_GetError();
-        }
+        sdlBlit(tiles[terrainIndex], x * hexSize * 0.75, mapHeight * hexSize);
     }
 
     SDL_UpdateRect(screen, 0, 0, 0, 0);
